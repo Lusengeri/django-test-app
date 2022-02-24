54272c75542c80aad1a3a6d35550c775b34168db
Lusengeri <lusengeri@gmail.com>
2022-02-24 19:47:00 +0300

    Set-up scripts and configuration

--- setup.sh
+++ setup.sh
@@ -2,11 +2,35 @@
 
 # Install the dependencies
 sudo apt update
-sudo apt install python3-pip python3-venv nginx uwsgi -y
+sudo apt install python3-dev python3-distutils python3-pip python3-venv nginx -y
 
 cd /home/ubuntu/
 git clone https://github.com/Lusengeri/django-test-app
 
-python3 -m venv .venv
-source /home/ubuntu/.env/bin/activate
-pip3 install -r /home/ubuntu/django-test-app/backend/requirements.txt
+sudo python3 -m venv .env
+sudo source /home/ubuntu/.env/bin/activate
+
+# Setup django application dependencies
+sudo pip3 install wheel
+sudo pip3 install uwsgi
+sudo pip3 install django
+sudo pip3 install -r /home/ubuntu/django-test-app/backend/requirements.txt
+
+# Get IP address
+MY_IP=`curl -s https://icanhazip.com`
+
+# Change ALLOWED_HOSTS[] in settings.py to ALLOWED_HOSTS = ['<my_ip_address>']
+sed -i "s/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = \['${MY_IP}'\]/" /home/ubuntu/django-test-app/backend/backend/settings.py
+
+# Set-up uwsgi
+# Create the socket location
+sudo mkdir -p /run/uwsgi/ && chown ubuntu:www-data /run/uwsgi/
+
+cd /home/ubuntu/django-test-app/
+uwsgi --ini /home/ubuntu/django-test-app/django-test-app.ini
+
+# Set-up nginx 
+# Insert the IP address in nginx site configuration file
+sudo sed 's/<my_ip_address>/'${MY_IP}'/' /home/ubuntu/django-test-app/django-test-app.conf > /etc/nginx/sites-available/django-test-app.conf
+sudo ln -s /etc/nginx/sites-available/django-test-app.conf /etc/nginx/sites-enabled/django-test-app.conf 
+sudo service nginx restart
