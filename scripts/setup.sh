f0ee2e232399804d645324a15ebf920af69c8065
Lusengeri <lusengeri@gmail.com>
2022-03-06 15:59:59 +0300

    Update setup.sh for CodeDeploy

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,9 +4,22 @@
 sudo apt update
 sudo apt install python3-dev gcc python3-distutils python3-pip python3-venv nginx -y
 
+# Install code-deploy agent
+sudo apt install ruby-full wget -y
+
 cd /home/ubuntu/
+
+# Install the CodeDeploy Agent 
+EC2_AVAIL_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
+EC2_REGION="`echo \"$EC2_AVAIL_ZONE\" | sed 's/[a-z]$//'`"
+wget https://aws-codedeploy-$EC2_REGION.s3.$EC2_REGION.amazonaws.com/latest/install
+chmod +x install
+sudo ./install auto
+
+# Clone the app repository
 git clone https://github.com/Lusengeri/django-test-app
 
+# Set up the Python virtual environment
 python3 -m venv .env
 source /home/ubuntu/.env/bin/activate
 
@@ -22,17 +35,6 @@ MY_IP=`curl -s https://icanhazip.com`
 # Change ALLOWED_HOSTS[] in settings.py to ALLOWED_HOSTS = ['<my_ip_address>']
 sudo sed -i "s/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = \['${MY_IP}'\]/" /home/ubuntu/django-test-app/backend/backend/settings.py
 
-# Set-up uwsgi
-# Create the socket location
-#sudo mkdir -p /run/uwsgi/ 
-#sudo chown ubuntu:www-data /run/uwsgi/
-#sudo chmod 664 /run/uwsgi/
-
-cd /home/ubuntu/django-test-app/backend/
-uwsgi --ini /home/ubuntu/django-test-app/backend/django-test-app.ini &
-
-sudo chown ubuntu:www-data /home/ubuntu/django-test-app/backend/django-test-app.sock
-
 # Set-up nginx 
 # And insert the IP address in nginx site configuration file
 sudo rm /etc/nginx/sites-available/default
@@ -42,3 +44,8 @@ sudo sed -i 's/<my_ip_address>/'${MY_IP}'/' /home/ubuntu/django-test-app/django-
 sudo cp /home/ubuntu/django-test-app/django-test-app.conf /etc/nginx/sites-available/django-test-app.conf
 sudo ln -s /etc/nginx/sites-available/django-test-app.conf /etc/nginx/sites-enabled/django-test-app.conf 
 sudo service nginx restart
+
+cd /home/ubuntu/django-test-app/backend/
+uwsgi --ini /home/ubuntu/django-test-app/backend/django-test-app.ini &
+
+sudo chown ubuntu:www-data /home/ubuntu/django-test-app/backend/django-test-app.sock
