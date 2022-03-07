b8f3c344935d41e6930f66653ccd98842cbaf3f2
Lusengeri <lusengeri@gmail.com>
2022-03-07 22:51:08 +0300

    application_start debug

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -5,6 +5,6 @@ sudo service nginx restart
 source /home/ubuntu/.env/bin/activate
 
 cd /home/ubuntu/django-test-app/backend/
-#uwsgi --ini /home/ubuntu/django-test-app/backend/django-test-app.ini && sudo chown ubuntu:www-data /home/ubuntu/django-test-app/backend/django-test-app.sock
+uwsgi --ini /home/ubuntu/django-test-app/backend/django-test-app.ini && sudo chown ubuntu:www-data /home/ubuntu/django-test-app/backend/django-test-app.sock
 
 echo "Completed 'application_start.sh'" >> /home/ubuntu/debug.log
