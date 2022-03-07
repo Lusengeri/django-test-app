d9c51a4e0ef34c323eb9a058e737f64c1c50723c
Lusengeri <lusengeri@gmail.com>
2022-03-07 19:03:01 +0300

    CD Scripts debug reversion

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-sudo service nginx restart
-source /home/ubuntu/.env/bin/activate
+echo "Starting 'application_start.sh...'" >> /home/ubuntu/debug.log
+#sudo service nginx restart
+#source /home/ubuntu/.env/bin/activate
 
-cd /home/ubuntu/django-test-app/backend/
-uwsgi --ini /home/ubuntu/django-test-app/backend/django-test-app.ini &
+#cd /home/ubuntu/django-test-app/backend/
+#uwsgi --ini /home/ubuntu/django-test-app/backend/django-test-app.ini &
 
-sudo chown ubuntu:www-data /home/ubuntu/django-test-app/backend/django-test-app.sock
+#sudo chown ubuntu:www-data /home/ubuntu/django-test-app/backend/django-test-app.sock
+echo "Completed 'application_start.sh'" >> /home/ubuntu/debug.log
--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+echo "Starting 'application_stop'..." >> /home/ubuntu/debug.log
 killall uwsgi
+echo "Completed 'application_stop'..." >> /home/ubuntu/debug.log
