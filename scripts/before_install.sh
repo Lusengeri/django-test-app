9a540935a7e41db59f8050464596d8ce87a0001c
Lusengeri <lusengeri@gmail.com>
2022-03-07 19:26:50 +0300

    Debug before_install CD script

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,10 +2,10 @@
 
 echo "Starting 'before_install.sh'..." >> /home/ubuntu/debug.log
 # Clean up previous installation
-#sudo rm -rf /home/ubuntu/django-test-app
-#sudo rm -rf /home/ubuntu/.env
-#sudo rm -f /etc/nginx/sites-available/default
-#sudo rm -f /etc/nginx/sites-enabled/default
-#sudo rm -f /etc/nginx/sites-available/django-test-app.conf
-#sudo rm -f /etc/nginx/sites-enabled/django-test-app.conf
+sudo rm -rf /home/ubuntu/django-test-app
+sudo rm -rf /home/ubuntu/.env
+sudo rm -f /etc/nginx/sites-available/default
+sudo rm -f /etc/nginx/sites-enabled/default
+sudo rm -f /etc/nginx/sites-available/django-test-app.conf
+sudo rm -f /etc/nginx/sites-enabled/django-test-app.conf
 echo "Completed 'before_install.sh'" >> /home/ubuntu/debug.log
