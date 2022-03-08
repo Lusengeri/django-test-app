df5035783a8bfe0cd82df18eaab10e46f0c7f7e2
Lusengeri <lusengeri@gmail.com>
2022-03-08 14:02:40 +0300

    Fix application_start: daemonize uwsgi

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -5,6 +5,9 @@ sudo service nginx restart
 source /home/ubuntu/.env/bin/activate
 
 cd /home/ubuntu/django-test-app/backend/
+
+mkdir -p /var/log/uwsgi/
+
 uwsgi --ini /home/ubuntu/django-test-app/backend/django-test-app.ini && sudo chown ubuntu:www-data /home/ubuntu/django-test-app/backend/django-test-app.sock
 
 echo "Completed 'application_start.sh'" >> /home/ubuntu/debug.log
