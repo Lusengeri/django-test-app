9e812e0a4223b852b6210cb3a8d51cb0bbf30cd7
Lusengeri <lusengeri@gmail.com>
2022-05-01 07:44:06 +0300

    Set-up base url for deployment

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -40,12 +40,12 @@ pip3 install django
 pip3 install -r /home/ubuntu/django-test-app/requirements.txt
 
 # Get IP address
-#MY_IP=`curl -s https://icanhazip.com`
+MY_IP=`curl -s https://icanhazip.com`
 
 # Change ALLOWED_HOSTS[] in settings.py to ALLOWED_HOSTS = ['<my_ip_address>']
 #sudo sed -i "s/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = \['${MY_IP}'\]/" /home/ubuntu/django-test-app/backend/backend/settings.py
 
-# Set-up nginx site configuration file  
+# Set-up nginx site configuration file
 #sudo sed -i 's/<my_ip_address>/'${MY_IP}'/' /home/ubuntu/django-test-app/django-test-app.conf
 sudo cp /home/ubuntu/django-test-app/django-test-app.conf /etc/nginx/sites-available/django-test-app.conf
 sudo ln -s /etc/nginx/sites-available/django-test-app.conf /etc/nginx/sites-enabled/django-test-app.conf 
@@ -62,18 +62,22 @@ sudo chown ubuntu:www-data /home/ubuntu/django-test-app/backend/django-test-app.
 
 # Set-up React front-end
 # Install node ppa
-#curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-#sudo apt install nodejs -y
+curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+sudo apt install nodejs -y
 
-# Set up Nodejs and Yarn
+# Set up Yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update
-sudo apt install yarn -y
+sudo apt install --no-install-recommends yarn -y
 
 # Move to frontend folder and install dependencies
 cd /home/ubuntu/django-test-app/frontend/
+
+# Set base url for production environment
+sudo sed -i "s/localhost:8000/${MY_IP}/" /home/ubuntu/django-test-app/frontend/src/App.js
+
 yarn install
 yarn build
 
-sudo cp /home/ubuntu/django-test-app/frontend/build/* /var/www/
+sudo cp -r /home/ubuntu/django-test-app/frontend/build/ /var/www/
