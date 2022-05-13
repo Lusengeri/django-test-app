bdd8909b46f790bc3545eb1a325079e26210168b
Lusengeri <lusengeri@gmail.com>
2022-05-13 19:01:20 +0300

    change database connection parameters

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -82,16 +82,16 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('DBNAME'),
-        'USER': os.getenv('DBUSER'),
-        'PASSWORD': os.getenv('DBPASSWORD'),
-        'HOST': os.getenv('DBHOST'),
-        'PORT': '5432',
+        #'ENGINE': 'django.db.backends.postgresql',
+        #'NAME': os.getenv('DBNAME'),
+        #'USER': os.getenv('DBUSER'),
+        #'PASSWORD': os.getenv('DBPASSWORD'),
+        #'HOST': os.getenv('DBHOST'),
+        #'PORT': '5432',
     }
 }
 
-#DATABASES ['default'] = dj_database_url.config(default=os.getenv('DATABASE_URL'))
+DATABASES ['default'] = dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
