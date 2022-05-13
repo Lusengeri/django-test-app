5b5761d41a811a09dd9a1dfc8e26a28a91982306
Lusengeri <lusengeri@gmail.com>
2022-05-13 22:54:15 +0300

    .

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
     }
 }
 
-DATABASES ['default'] = dj_database_url.parse(os.getenv('DATABASE_URL'), conn_max_age=600)
+DATABASES ['default'] = dj_database_url.config(default=os.getenv('DATABASE_URL'), conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
