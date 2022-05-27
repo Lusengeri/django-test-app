733076a80d719bee002f2aa1fa724bc340f6f323
Lusengeri <lusengeri@gmail.com>
2022-05-27 07:53:41 +0300

    reorganize config and setup files

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,20 +9,20 @@ pip3 install --upgrade pip
 # Install dependencies for code-deploy agent
 sudo apt install ruby-full wget -y
 
-# Move to home directory
-cd /home/ubuntu/
+# Move to source code directory
+cd /usr/src/
 
 # Install the CodeDeploy Agent 
 EC2_AVAIL_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
 EC2_REGION="`echo \"$EC2_AVAIL_ZONE\" | sed 's/[a-z]$//'`"
-wget https://aws-codedeploy-$EC2_REGION.s3.$EC2_REGION.amazonaws.com/latest/install
-chmod +x install
+sudo wget https://aws-codedeploy-$EC2_REGION.s3.$EC2_REGION.amazonaws.com/latest/install
+sudo chmod +x install
 sudo ./install auto
 
 # Clean up previous installation (if any)
-sudo rm -rf /home/ubuntu/django-test-app
-sudo rm -rf /home/ubuntu/.env
-sudo rm -rf /home/ubuntu/install
+sudo rm -rf /usr/src/django-test-app
+sudo rm -rf /usr/src/.env
+sudo rm -rf /usr/src/install
 
 sudo rm -f /etc/nginx/sites-available/default
 sudo rm -f /etc/nginx/sites-enabled/default
@@ -62,25 +62,25 @@ export DBPORT=5432
 export DATABASE_URL=postgres://$DB_USERNAME:$DB_PASSWORD@$1:5432/taskmanagerdb
 
 # Clone the app repository
-git clone https://github.com/Lusengeri/django-test-app
-sudo chown -R ubuntu:www-data /home/ubuntu/django-test-app
+sudo git clone https://github.com/Lusengeri/django-test-app
+sudo chown -R ubuntu:www-data /usr/src/django-test-app
 
 # Set up the Python virtual environment
 python3 -m venv .env
-source /home/ubuntu/.env/bin/activate
+source /usr/src/.env/bin/activate
 
 # Setup django application dependencies
 pip3 install wheel 
 pip3 install uwsgi
 pip3 install django
-pip3 install -r /home/ubuntu/django-test-app/backend/requirements.txt
+pip3 install -r /usr/src/django-test-app/backend/requirements.txt
 
 # Database migration
-python3 /home/ubuntu/django-test-app/backend/manage.py makemigrations
-python3 /home/ubuntu/django-test-app/backend/manage.py migrate
+python3 /usr/src/django-test-app/backend/manage.py makemigrations
+python3 /usr/src/django-test-app/backend/manage.py migrate
 
 # Set-up nginx site configuration file
-sudo cp /home/ubuntu/django-test-app/django-test-app.conf /etc/nginx/sites-available/django-test-app.conf
+sudo cp /usr/src/django-test-app/config/nginx.conf /etc/nginx/sites-available/django-test-app.conf
 sudo ln -s /etc/nginx/sites-available/django-test-app.conf /etc/nginx/sites-enabled/django-test-app.conf 
 sudo service nginx restart
 
@@ -91,8 +91,8 @@ sudo chown ubuntu:www-data /var/log/uwsgi/
 sudo mkdir -p /run/uwsgi/
 sudo chown ubuntu:www-data /run/uwsgi/
 
-cd /home/ubuntu/django-test-app/backend/
-uwsgi --ini /home/ubuntu/django-test-app/backend/django-test-app.ini
+cd /usr/src/django-test-app/backend/
+uwsgi --ini /usr/src/django-test-app/backend/uwsgi.ini
 
 sudo chown ubuntu:www-data /run/uwsgi/django-test-app.sock
 
@@ -112,15 +112,15 @@ sudo apt install --no-install-recommends yarn -y
 MY_DOMAIN=$2
 
 # Set base url for production environment
-sudo sed -i "s/localhost:8000/$MY_DOMAIN/" /home/ubuntu/django-test-app/frontend/src/App.js
+sudo sed -i "s/localhost:8000/$MY_DOMAIN/" /usr/src/django-test-app/frontend/src/App.js
 
 # Move to frontend folder, install dependencies and create production build
-cd /home/ubuntu/django-test-app/frontend/
-yarn install
-yarn build
+cd /usr/src/django-test-app/frontend/
+sudo yarn install
+sudo yarn build
 
 # Ensure availability of Django static files on /static/ prefix
-for dir in /home/ubuntu/django-test-app/backend/static/*
+for dir in /usr/src/django-test-app/backend/static/*
 do
-	ln -s "$dir" /home/ubuntu/django-test-app/frontend/build/static/
+	ln -s "$dir" /usr/src/django-test-app/frontend/build/static/
 done
